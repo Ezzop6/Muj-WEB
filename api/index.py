8315a0cbf953ecf321af655a472b263761dca327
Ezzop6 <ezzop6gmail.com>
2023-03-13 14:08:28 +0100

    dalsi pokus o spravnou strukturu

--- api/index.py
+++ api/index.py
@@ -1,9 +1,9 @@
 from flask import Flask
 import os
-from my_packages import _tools
-from bl_pages import main_pages
+from my_packages._tools import *
+from bl_pages.main_pages import main_pages
 
-bl_main_pages = main_pages.main_pages
+bl_main_pages = main_pages
 
 app = Flask(__name__)
 app.register_blueprint(bl_main_pages, url_prefix='/')
@@ -14,5 +14,5 @@ if __name__ == '__main__':
     host = os.getenv('IP', '0.0.0.0')
     port = int(os.getenv('PORT', 3000))
     app.debug = True #TODO remove this after development
-    app.jinja_env.globals.update(get_random_produkt_img=_tools.get_random_produkt_img)
+    app.jinja_env.globals.update(get_random_produkt_img=get_random_produkt_img)
     app.run(host=host, port=port)
