4143584ad0755eaf6c32c930315f80e002f9b2d1
Ezzop6 <ezzop6gmail.com>
2023-03-13 14:20:57 +0100

    dalsi pokus

--- api/index.py
+++ api/index.py
@@ -1,7 +1,15 @@
+debug = False #TODO remove this after development
+
+if debug:
+    from my_packages._tools import *
+    from bl_pages.main_pages import main_pages
+else:
+    from api.my_packages._tools import *
+    from api.bl_pages.main_pages import main_pages
+
+
 from flask import Flask
 import os
-from my_packages._tools import *
-from bl_pages.main_pages import main_pages
 
 bl_main_pages = main_pages
 
@@ -13,6 +21,7 @@ app.register_blueprint(bl_main_pages, url_prefix='/')
 if __name__ == '__main__':
     host = os.getenv('IP', '0.0.0.0')
     port = int(os.getenv('PORT', 3000))
-    app.debug = True #TODO remove this after development
+    if debug:
+        app.debug = True
     app.jinja_env.globals.update(get_random_produkt_img=get_random_produkt_img)
     app.run(host=host, port=port)
