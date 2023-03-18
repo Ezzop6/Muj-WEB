be67c4ea03b5173ded29a75c4d4b361f3ec5f663
Ezzop6 <ezzop6gmail.com>
2023-03-18 15:29:37 +0100

    nastilovan kalendar

--- api/forms/smenost_app/forms.py
+++ api/forms/smenost_app/forms.py
@@ -4,21 +4,15 @@ import os
 load_dotenv(find_dotenv())
 debug = os.environ.get("DEBUG") == "True"
 
-if debug:
-    from database import DbUsersMain
-else:
-    from api.database import DbUsersMain
-    
 from flask_wtf import FlaskForm
 from wtforms import  SelectField, SubmitField
 
 import datetime
 
-db = DbUsersMain()
-
+months = [(1, 'Leden'), (2, 'Únor'), (3, 'Březen'), (4, 'Duben'), (5, 'Květen'), (6, 'Červen'), (7, 'Červenec'), (8, 'Srpen'), (9, 'Září'), (10, 'Říjen'), (11, 'Listopad'), (12, 'Prosinec')]
 class Kalendar(FlaskForm):
-    year = SelectField('Rok', choices=[(year, year) for year in range(2019, datetime.date.today().year + 5)],
+    year = SelectField('Rok', choices=[(year, year) for year in range(2023, datetime.date.today().year + 5)],
         default = datetime.date.today().year)
-    month = SelectField('Měsíc', choices=[(month, month) for month in range(1, 13)],
+    month = SelectField('Měsíc', choices=[month for month in months],
         default = datetime.date.today().month)
     submit = SubmitField('Zobrazit')
