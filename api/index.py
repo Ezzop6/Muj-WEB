c38bdae85271c41d3098d69072ecc9523e7f5e38
Ezzop6 <ezzop6gmail.com>
2023-03-18 16:49:39 +0100

    opraven kalendar uz neukazuje dny co neexistujou

--- api/index.py
+++ api/index.py
@@ -23,7 +23,7 @@ else:
 
 from flask_session import Session
 from flask import Flask, render_template, redirect, url_for, g, session
-from flask_login import current_user, LoginManager, UserMixin, login_user, logout_user
+from flask_login import LoginManager, UserMixin, login_user, logout_user
 from flask_wtf.csrf import CSRFProtect
 from redis import Redis
 import os
